102a5feb4fe299c3d84406516fc4895ce00ce684
Matt Kunnari <mkunnari1@gmail.com>
2019-07-25 17:46:58 -0700

    Updates to Django Project

--- Django Project/techproject/src/mainapp/mainapp/views.py
+++ Django Project/techproject/src/mainapp/mainapp/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    user = request.user
-    context = (
-        'user': user,
-    )
-    return render(request, 'home.html', context)
 
+def home(request):
+    products = ["Cherries", "Apples", "Oranges", "Strawberries", "Pears", "Watermelons"]
+    context = {
+        'products': products,
+    }
+    return render(request, "home.html", context)
