37d1a1f9e9559321c70b156c54218e5740a28fb1
Matt Kunnari <mkunnari1@gmail.com>
2019-07-27 19:28:30 -0700

    Updates for Django project

--- Django Project/techproject/src/mainapp/products/views.py
+++ Django Project/techproject/src/mainapp/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from.models import Product
+
+def admin_console(request):
+    products = Product.objects.all()
+    return render(request, 'products/products_page.html', {'products' : products})
