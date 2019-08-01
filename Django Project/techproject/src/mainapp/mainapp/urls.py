cfe2a3beb56a3a96a27be795e199acddab706313
Matt Kunnari <mkunnari1@gmail.com>
2019-07-31 17:32:01 -0700

    Updates to django project

--- Django Project/techproject/src/mainapp/mainapp/urls.py
+++ Django Project/techproject/src/mainapp/mainapp/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
